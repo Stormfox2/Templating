aaf12c1049087038f843bce825f2725972598bef
Quirin Brändli <qbraendli@pm.me>
2023-12-08 10:24:02 +0100

    first commit

---
+++ create_template.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1454ec1e92169d882acebc516d800d58fe7536d145ee3037c90352a8aab752f8
+size 302
---
+++ reload_template.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b1b00c9992ca5e98fdb2c26161043741360cbea436cdf57c96841064d55d5579
+size 281
