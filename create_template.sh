dd076c5ec1961b3da5b05ba928cc15bf0a3bc0d1
root <root@host01.int.ketarion.de>
2023-12-13 09:59:13 +0100

    Adding nano and home directory

--- create_template.sh
+++ create_template.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1454ec1e92169d882acebc516d800d58fe7536d145ee3037c90352a8aab752f8
-size 302
+oid sha256:b659ef8896668000ac67000c7803feb5627acfdfbb942b43978e1ebb97cad61e
+size 362
