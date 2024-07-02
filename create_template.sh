ce8b9692b3049aee980de91273ef50849200c775
root <root@host01.ketarion.de>
2024-07-02 15:16:57 +0200

    New R9 image

--- create_template.sh
+++ create_template.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b659ef8896668000ac67000c7803feb5627acfdfbb942b43978e1ebb97cad61e
-size 362
+oid sha256:be7dc4628dd832fccd0473d84923bddf5b7f8ae9a17782429c09c24f8d30b0a3
+size 359
